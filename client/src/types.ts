4bb7b2154402281025c6c3ac7e4906f34c96a5cf
daniele24134 <capanodaniele@outlook.it>
2022-04-20 12:13:41 +0200

    added types

---
+++ client/src/types.ts
@@ -0,0 +1,34 @@
+
+export type Beer = {
+  id: number,
+  name: string,
+  tagline: string,
+  description:string,
+  image_url: string,
+  abv: number,
+  ibu: number,
+  ebc: number,
+  ingredients: {
+    malt: Malt[],
+    hops: Hop[]
+  },
+  food_pairing: string[]
+}
+
+export type Malt =  {
+  name: string,
+  amount : {
+    value: number,
+    unit: string
+  }
+}
+
+export type Hop = {
+  name: string,
+  amount: {
+    value: number,
+    unit: string
+  },
+  add: string,
+  attribute: string
+}
